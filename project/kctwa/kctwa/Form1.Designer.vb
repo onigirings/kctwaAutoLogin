9e626ef47dc349e9ce14c577b8f5fa99fd3d233a
mrtb <onigirings@gmail.com>
2017-06-03 20:49:18 +0900

    first commit

---
+++ project/kctwa/kctwa/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox_remember = New System.Windows.Forms.CheckBox()
+        Me.Button_stop = New System.Windows.Forms.Button()
+        Me.Button_run = New System.Windows.Forms.Button()
+        Me.TextBox_PW = New System.Windows.Forms.TextBox()
+        Me.TextBox_ID = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button_close = New System.Windows.Forms.Button()
+        Me.BackGroundBrowser = New System.Windows.Forms.WebBrowser()
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip_n = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Menu_option = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Menu_exit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CycleCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.ContextMenuStrip_n.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox_remember)
+        Me.GroupBox1.Controls.Add(Me.Button_stop)
+        Me.GroupBox1.Controls.Add(Me.Button_run)
+        Me.GroupBox1.Controls.Add(Me.TextBox_PW)
+        Me.GroupBox1.Controls.Add(Me.TextBox_ID)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(229, 176)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'CheckBox_remember
+        '
+        Me.CheckBox_remember.AutoSize = True
+        Me.CheckBox_remember.Location = New System.Drawing.Point(108, 102)
+        Me.CheckBox_remember.Name = "CheckBox_remember"
+        Me.CheckBox_remember.Size = New System.Drawing.Size(97, 16)
+        Me.CheckBox_remember.TabIndex = 6
+        Me.CheckBox_remember.Text = "Remember me"
+        Me.CheckBox_remember.UseVisualStyleBackColor = True
+        '
+        'Button_stop
+        '
+        Me.Button_stop.Enabled = False
+        Me.Button_stop.Location = New System.Drawing.Point(130, 135)
+        Me.Button_stop.Name = "Button_stop"
+        Me.Button_stop.Size = New System.Drawing.Size(75, 23)
+        Me.Button_stop.TabIndex = 5
+        Me.Button_stop.Text = "stop"
+        Me.Button_stop.UseVisualStyleBackColor = True
+        '
+        'Button_run
+        '
+        Me.Button_run.Location = New System.Drawing.Point(25, 135)
+        Me.Button_run.Name = "Button_run"
+        Me.Button_run.Size = New System.Drawing.Size(75, 23)
+        Me.Button_run.TabIndex = 4
+        Me.Button_run.Text = "run"
+        Me.Button_run.UseVisualStyleBackColor = True
+        '
+        'TextBox_PW
+        '
+        Me.TextBox_PW.Location = New System.Drawing.Point(75, 66)
+        Me.TextBox_PW.Name = "TextBox_PW"
+        Me.TextBox_PW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(64)
+        Me.TextBox_PW.Size = New System.Drawing.Size(130, 19)
+        Me.TextBox_PW.TabIndex = 3
+        '
+        'TextBox_ID
+        '
+        Me.TextBox_ID.Location = New System.Drawing.Point(75, 28)
+        Me.TextBox_ID.Name = "TextBox_ID"
+        Me.TextBox_ID.Size = New System.Drawing.Size(130, 19)
+        Me.TextBox_ID.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "PW"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'Button_close
+        '
+        Me.Button_close.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button_close.Location = New System.Drawing.Point(166, 200)
+        Me.Button_close.Name = "Button_close"
+        Me.Button_close.Size = New System.Drawing.Size(75, 23)
+        Me.Button_close.TabIndex = 1
+        Me.Button_close.Text = "close"
+        Me.Button_close.UseVisualStyleBackColor = True
+        '
+        'BackGroundBrowser
+        '
+        Me.BackGroundBrowser.Location = New System.Drawing.Point(265, 12)
+        Me.BackGroundBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.BackGroundBrowser.Name = "BackGroundBrowser"
+        Me.BackGroundBrowser.Size = New System.Drawing.Size(116, 100)
+        Me.BackGroundBrowser.TabIndex = 2
+        Me.BackGroundBrowser.Visible = False
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "kctwa"
+        Me.NotifyIcon.Visible = True
+        '
+        'ContextMenuStrip_n
+        '
+        Me.ContextMenuStrip_n.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Menu_option, Me.Menu_exit})
+        Me.ContextMenuStrip_n.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip_n.Size = New System.Drawing.Size(99, 48)
+        '
+        'Menu_option
+        '
+        Me.Menu_option.Name = "Menu_option"
+        Me.Menu_option.Size = New System.Drawing.Size(98, 22)
+        Me.Menu_option.Text = "設定"
+        '
+        'Menu_exit
+        '
+        Me.Menu_exit.Name = "Menu_exit"
+        Me.Menu_exit.Size = New System.Drawing.Size(98, 22)
+        Me.Menu_exit.Text = "終了"
+        '
+        'CycleCounter
+        '
+        Me.CycleCounter.Interval = 5000
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button_run
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Button_close
+        Me.ClientSize = New System.Drawing.Size(253, 235)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.BackGroundBrowser)
+        Me.Controls.Add(Me.Button_close)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "kctwa"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ContextMenuStrip_n.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button_stop As Button
+    Friend WithEvents Button_run As Button
+    Friend WithEvents TextBox_PW As TextBox
+    Friend WithEvents TextBox_ID As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button_close As Button
+    Friend WithEvents BackGroundBrowser As WebBrowser
+    Friend WithEvents NotifyIcon As NotifyIcon
+    Friend WithEvents ContextMenuStrip_n As ContextMenuStrip
+    Friend WithEvents CycleCounter As Timer
+    Friend WithEvents Menu_option As ToolStripMenuItem
+    Friend WithEvents Menu_exit As ToolStripMenuItem
+    Friend WithEvents CheckBox_remember As CheckBox
+End Class
---
+++ project/kctwa/kctwa/Form1.vb
@@ -0,0 +1,226 @@
+﻿Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports Microsoft.Win32
+Imports System.Management
+Imports System.Text
+Imports NativeWifi
+
+Public Class Form1
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.remember_me = True Then
+            TextBox_ID.Text = My.Settings.remember_ID
+            TextBox_PW.Text = My.Settings.remember_PW
+            CheckBox_remember.CheckState = CheckState.Checked
+        End If
+        '       System.Windows.Forms.NotifyIcon.ContextMenuStrip = ContextMenuStrip_n
+
+    End Sub
+
+    Sub run_click()
+        CycleCounter.Start()
+        Button_run.Enabled = False
+        Button_stop.Enabled = True
+        NotifyIcon.Visible = True
+        TextBox_ID.Enabled = False
+        TextBox_PW.Enabled = False
+    End Sub
+
+    Sub stop_click()
+        CycleCounter.Stop()
+        Button_stop.Enabled = False
+        Button_run.Enabled = True
+        NotifyIcon.Visible = False
+        TextBox_ID.Enabled = True
+        TextBox_PW.Enabled = True
+    End Sub
+
+    Private Sub Button_run_Click(sender As Object, e As EventArgs) Handles Button_run.Click
+        run_click()
+    End Sub
+
+    Private Sub Button_stop_Click(sender As Object, e As EventArgs) Handles Button_stop.Click
+        stop_click()
+    End Sub
+
+    Private Sub Button_close_Click(sender As Object, e As EventArgs) Handles Button_close.Click
+        My.Settings.remember_me = CheckBox_remember.CheckState
+        If CheckBox_remember.CheckState = CheckState.Checked Then
+            My.Settings.remember_ID = TextBox_ID.Text
+            My.Settings.remember_PW = TextBox_PW.Text
+        End If
+        If Button_run.Enabled = False Then
+            Visible = False
+            With NotifyIcon
+                .BalloonTipTitle = "バックグラウンドで動作中"
+                .BalloonTipText = "バックグラウンドで動作中"
+                .BalloonTipIcon = ToolTipIcon.Info
+                .ShowBalloonTip(5000)
+            End With
+        Else
+
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub NotifyIcon_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon.MouseDoubleClick
+        Visible = True
+    End Sub
+
+    Private Sub Menu_option_Click(sender As Object, e As EventArgs) Handles Menu_option.Click
+        Visible = True
+    End Sub
+
+    Private Sub Menu_exit_Click(sender As Object, e As EventArgs) Handles Menu_exit.Click
+        Application.Exit()
+    End Sub
+
+    Dim counter As Integer = 0
+    Private Sub BackGroundBrowser_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles BackGroundBrowser.DocumentCompleted
+
+        '    MsgBox("AAA")
+        Dim a As Object = SubmitProcedure()
+
+    End Sub
+
+    Dim SP_judge As Boolean = False
+
+    Private Function SubmitProcedure()
+        SP_judge = False
+        Try
+            Dim all As HtmlElementCollection = BackGroundBrowser.Document.All
+            Dim UserNameForm As HtmlElementCollection = all.GetElementsByName("username")
+            Dim PassWordForm As HtmlElementCollection = all.GetElementsByName("password")
+            Dim SubmitButton As HtmlElementCollection = all.GetElementsByName("Submit")
+            UserNameForm(0).InnerText = TextBox_ID.Text
+            PassWordForm(0).InnerText = TextBox_PW.Text
+            'MsgBox(SubmitButton.Count)
+            SubmitButton(0).InvokeMember("click")
+            'BackGroundBrowser.Document.InvokeScript("submitAction")
+            SP_judge = True
+
+        Catch ex As System.ArgumentOutOfRangeException
+            If SP_judge <> False Then
+                SP_judge = False
+            End If
+        End Try
+    End Function
+
+    Private Function LogoutProcedure()
+
+        Dim all As HtmlElementCollection = BackGroundBrowser.Document.All
+        Dim LogoutButton As HtmlElementCollection = all.GetElementsByName("Logout")
+        LogoutButton(0).InvokeMember("click")
+    End Function
+
+    Dim Info_num As Integer = -1
+
+    Private Sub CycleCounter_Tick(sender As Object, e As EventArgs) Handles CycleCounter.Tick
+
+        Dim AP_judge As Boolean = False
+        Dim WI_judge As Boolean = False
+        Try
+            Dim wlan = New WlanClient()
+            For Each wlanInterface As WlanClient.WlanInterface In wlan.Interfaces
+                Dim ssid As Wlan.Dot11Ssid = wlanInterface.CurrentConnection.wlanAssociationAttributes.dot11Ssid
+                If "kctwa".Equals(New [String](Encoding.ASCII.GetChars(ssid.SSID, 0, CInt(ssid.SSIDLength)))) Then
+                    AP_judge = True
+                End If
+            Next
+            WI_judge = True
+        Catch ex As System.ComponentModel.Win32Exception
+            AP_judge = True
+            If WI_judge <> False Then
+                WI_judge = False
+                Info_num = 2
+            End If
+        End Try
+
+        If (Not AP_judge) Then 'kctwaに繋がってない
+            If (Info_num <> 0) Then
+                Info_num = 0
+                With NotifyIcon
+                    .BalloonTipTitle = "ステータス：　kctwa未接続"
+                    .BalloonTipText = "繋がってない"
+                    .BalloonTipIcon = ToolTipIcon.Error
+                    .ShowBalloonTip(5000)
+
+                End With
+            End If
+        Else 'kctwaにつながってる
+            Try
+                Dim p As New System.Net.NetworkInformation.Ping()
+                Dim reply As System.Net.NetworkInformation.PingReply = p.Send("1.1.1.1", 1000)
+                If (reply.Status <> System.Net.NetworkInformation.IPStatus.Success) Then
+                    'ここにログイン処理書く
+                    If (Info_num <> 1) Then
+                        Info_num = 1
+                        With NotifyIcon
+                            .BalloonTipTitle = "ステータス：　未ログイン状態"
+                            .BalloonTipText = "kctwaのログイン処理をしています..."
+                            .BalloonTipIcon = ToolTipIcon.Info
+                            .ShowBalloonTip(300)
+                        End With
+                        SP_judge = False
+                    End If
+                    If SP_judge = False Then
+                        SP_judge = True
+                        BackGroundBrowser.Navigate("https://1.1.1.1/login.html", False)
+                    End If
+                Else
+                    'ここはログイン後
+                    If (Info_num <> 2) Then
+                        Info_num = 2
+                        With NotifyIcon
+                            .BalloonTipTitle = "ステータス：　ログイン成功"
+                            .BalloonTipText = "kctwaにログインしました"
+                            .BalloonTipIcon = ToolTipIcon.Info
+                            .ShowBalloonTip(300)
+                        End With
+                    End If
+                End If
+
+            Catch ex As System.Net.NetworkInformation.PingException
+            End Try
+        End If
+    End Sub
+
+    Private Sub NotifyIcon_BallonTipClicked(sender As Object, e As EventArgs) Handles NotifyIcon.BalloonTipClicked
+        Visible = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler SystemEvents.PowerModeChanged, AddressOf SystemEvents_PowerModeChanged
+    End Sub
+
+    Private Sub SystemEvents_PowerModeChanged(ByVal sender As Object, ByVal e As PowerModeChangedEventArgs)
+        Select Case e.Mode
+            Case PowerModes.Suspend
+                stop_click()
+            Case PowerModes.Resume
+                run_click()
+        End Select
+
+    End Sub
+
+    Private Sub BackGroundBrowser_NewWindow(sender As Object, e As CancelEventArgs) Handles BackGroundBrowser.NewWindow
+        'MsgBox("YOYO")
+        e.Cancel = True
+    End Sub
+
+End Class
+
+Namespace My
+
+    Partial Friend Class MyApplication
+
+        Private Sub MyApplication_Shutdown(ByVal sender As Object,
+            ByVal e As EventArgs) Handles Me.Shutdown
+
+            MsgBox("アプリケーションが終了します。")
+            Form1.NotifyIcon.Visible = False
+        End Sub
+
+    End Class
+
+End Namespace
